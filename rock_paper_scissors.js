b537570451d2f797abb2f290e09b3a6f3dc2d016
steffano0 <steffanouromero@gmail.com>
2023-02-27 18:50:24 -0500

    Add functionality to the game

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -2,12 +2,72 @@ function getComputerChoice() {
     let optionNumber = Math.floor(Math.random() * 3)
     switch (optionNumber){
         case 0 :
-            return "rock"
+            return "rock";
         case 1:
-            return "paper"
+            return "paper";
         case 2:
-            return "scissors"
+            return "scissors";
     }
 }
+
+function playRound (playerSelection, computerSelection) {
+    
+    switch (true) {
+
+        case (playerSelection === "rock" && computerSelection === "paper"):
+            alert("paper beats rock");
+            return "You lose!";
+        case (playerSelection === "paper" && computerSelection === "scissors"):
+            alert("scissors beat paper");
+            return "You lose!";
+        case (playerSelection === "scissors" && computerSelection === "rock"):
+            alert("rock beat scissors");
+            return "You lose!";
+        case (playerSelection === "paper" && computerSelection === "rock"):
+            alert("paper beats rock");
+            return "You win!";
+        case (playerSelection === "scissors" && computerSelection === "paper"):
+            alert("scissors beat paper");
+            return "You win!";
+        case (playerSelection === "rock" && computerSelection === "scissors"):
+            alert("rock beat scissors");
+            return "You win!";
+        default:
+            return "It's a tie"
+    }
+}
+
+
+
+
+function game() {
+    let computerScore = 0;
+    let playerScore = 0;
+    
+    for (let i = 0; i < 5; i++) { 
+        const computerSelection = getComputerChoice();
+        const playerSelection = prompt("To play enter rock, paper or scissors:").toLowerCase();
+
+        if (playRound(playerSelection, computerSelection) === "You win!") {
+            playerScore++;
+            alert(`You win ! the ${i + 1} round `)
+        } else if (playRound(playerSelection, computerSelection) === "You lose!") {
+            computerScore++;
+            alert(`You lose! the ${i + 1} round `)
+        } else {
+            alert("It's a tie!")
+        }
+    }
+
+    if (playerScore > computerScore) {
+        alert(`The computer suck! Player wins ${playerScore} - ${computerScore}`)
+    } else if (playerScore < computerScore) {
+        alert(`You suck! Computer wins ${computerScore} - ${playerScore}`)
+    } else {
+        alert(`Both of you suck! ${computerScore} - ${playerScore}`)
+    }
+
+}
+console.log(game()) 
 
 
