e32e4d5457971f7d497c79435aaaa263f66d0953
steffano0 <steffanouromero@gmail.com>
2023-03-08 00:34:49 -0500

    Add functionality to the game

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,73 +1,85 @@
 function getComputerChoice() {
-    let optionNumber = Math.floor(Math.random() * 3)
-    switch (optionNumber){
-        case 0 :
-            return "rock";
-        case 1:
-            return "paper";
-        case 2:
-            return "scissors";
-    }
+    let optionNumber = Math.floor(Math.random() * 3);
+    const options = ["rock", "paper", "scissors"];
+    return options[optionNumber];
+    
 }
 
 function playRound (playerSelection, computerSelection) {
     
+    
+    
     switch (true) {
 
         case (playerSelection === "rock" && computerSelection === "paper"):
-            alert("paper beats rock");
-            return "You lose!";
+            text.textContent ="That's too bad! paper beats rock";
+            return 0
+            
         case (playerSelection === "paper" && computerSelection === "scissors"):
-            alert("scissors beat paper");
-            return "You lose!";
+            text.textContent ="Bad election! scissors beat paper";
+            return 0
+            
         case (playerSelection === "scissors" && computerSelection === "rock"):
-            alert("rock beat scissors");
-            return "You lose!";
+            text.textContent ="Oh noo! rock beat scissors";
+            return  0
         case (playerSelection === "paper" && computerSelection === "rock"):
-            alert("paper beats rock");
-            return "You win!";
+            text.textContent ="You're really good! paper beats rock";
+            return 1
         case (playerSelection === "scissors" && computerSelection === "paper"):
-            alert("scissors beat paper");
-            return "You win!";
+            text.textContent ="Nice election! Scissors beat paper";
+            return 1
         case (playerSelection === "rock" && computerSelection === "scissors"):
-            alert("rock beat scissors");
-            return "You win!";
+            text.textContent ="You rock! Rock beat scissors";
+            return 1
         default:
-            return "It's a tie"
+            text.textContent ="It's a tie";
+            
     }
 }
 
+function gameisOver() {
+    return (playerScore == 5 || computerScore == 5);
+
+}
+
+function initialState() {
+    computerScore = 0;
+    playerScore = 0;
+    text.textContent = "*Select an option to play";
+    score.textContent = "0 - 0";
+    
+    
+
+
+}
+
+function updateScore(playerScore, computerScore) {
+    score.textContent = `${playerScore} - ${computerScore}`
+}
+
+
+let computerScore = 0;
+let playerScore = 0;
+const text = document.querySelector(".text");
+const score = document.querySelector(".score");
+const btns = Array.from(document.querySelectorAll('button'));
+btns.forEach(btn => btn.addEventListener('click', game));
 
 
 
 function game() {
-    let computerScore = 0;
-    let playerScore = 0;
+    const playerSelection = this.className
+    const computerSelection = getComputerChoice();
+
+    if (gameisOver()) {
+        initialState();
+    } 
+    playRound(playerSelection, computerSelection) ? playerScore++ : computerScore++;
+    updateScore(playerScore, computerScore);
+
+
     
-    for (let i = 0; i < 5; i++) { 
-        const computerSelection = getComputerChoice();
-        const playerSelection = prompt("To play enter rock, paper or scissors:").toLowerCase();
 
-        if (playRound(playerSelection, computerSelection) === "You win!") {
-            playerScore++;
-            alert(`You win ! the ${i + 1} round `)
-        } else if (playRound(playerSelection, computerSelection) === "You lose!") {
-            computerScore++;
-            alert(`You lose! the ${i + 1} round `)
-        } else {
-            alert("It's a tie!")
-        }
-    }
-
-    if (playerScore > computerScore) {
-        alert(`The computer suck! Player wins ${playerScore} - ${computerScore}`)
-    } else if (playerScore < computerScore) {
-        alert(`You suck! Computer wins ${computerScore} - ${playerScore}`)
-    } else {
-        alert(`Both of you suck! ${computerScore} - ${playerScore}`)
-    }
-
-}
-console.log(game()) 
+}     
 
 
